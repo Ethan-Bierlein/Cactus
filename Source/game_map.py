af26d1cff94bdf0f8015ee25e82c55573797c028
ShearOfDoom <ebr4d@live.com>
2015-06-04 16:38:39 -0500

    Added game_map.py [UNTESTED]

---
+++ Source/game_map.py
@@ -0,0 +1,28 @@
+class GameMap(object):
+	"""
+	Stores general data about a game map. A GameMap
+	instance contains the following data.
+	
+		map_data - A list of MapPosition instances representing a game map.
+	"""
+	def __init__(self, map_data: list):
+		self.map_data = map_data
+		
+	def _find_start(self, lower_text: bool):
+		"""
+		This iterates over self.map_data and finds the MapPosition
+		instance with the name of start, and returns it's index.
+		"""
+		for index, map_position in enumerate(self.map_data):
+			if lower_text:
+				if self.map_position.name.lower() == "start":
+					return index
+			else:
+				if self.map_position.name == "start":
+					return index
+					
+	def _return_map_data(self):
+		"""
+		Returns self.map_data as a list.
+		"""
+		return self.map_data
