d8bcfccd26ff0c83f59f70ebd39da95eea9d3ee2
Annonomus Penguin <annonomus-penguin@users.noreply.github.com>
2015-06-07 11:28:49 -0500

    Tabs → spaces

--- CactusSource/game_map.py
+++ CactusSource/game_map.py
@@ -1,22 +1,22 @@
 class GameMap(object):
-	"""
-	Stores general data about a game map. A GameMap
-	instance contains the following data.
-	
-		data - A list of MapPosition instances representing a game map.
-	"""
-	def __init__(self, data: list):
-		self.data = data
-		
-	def find_start(self, should_lower_text: bool):
-		"""
-		This iterates over self.data and finds the MapPosition
-		instance with the name of start, and returns it's index.
-		"""
-		for index, map_position in enumerate(self.data):
-			if should_lower_text:
-				if map_position.name.lower() == "start":
-					return index
-			else:
-				if self.map_position.name == "start":
-					return index
+    """
+    Stores general data about a game map. A GameMap
+    instance contains the following data.
+    
+        data - A list of MapPosition instances representing a game map.
+    """
+    def __init__(self, data: list):
+        self.data = data
+        
+    def find_start(self, should_lower_text: bool):
+        """
+        This iterates over self.data and finds the MapPosition
+        instance with the name of start, and returns it's index.
+        """
+        for index, map_position in enumerate(self.data):
+            if should_lower_text:
+                if map_position.name.lower() == "start":
+                    return index
+            else:
+                if self.map_position.name == "start":
+                    return index
--- CactusSource/main_game.py
+++ CactusSource/main_game.py
@@ -1,44 +1,44 @@
 class MainGame(object):
-	"""
-	Stores general data about the game. A MainGame instance
-	contains the following attributes.
-	
-		name              - The name of the game.
-		desc              - The game's description.
-		prompt            - The global prompt to be used.
-		invalid_input_msg - A message to print if user enters invalid input.
-		map               - The game's map data, a GameMap instance.
-		should_lower_text - The option to lower text or not.
-	"""
-	def __init__(self, name: str, desc: str, prompt: str, invalid_input_msg: str, map, should_lower_text: bool):
-		self.name              = name
-		self.desc              = desc
-		self.prompt            = prompt
-		self.invalid_input_msg = invalid_input_msg
-		self.map               = map
-		self.should_lower_text = should_lower_text
-		self.map_start         = self.map.find_start(self.should_lower_text)
-		self.map_position      = self.map_start
-		
-	def _conditional_lower(self, text: str):
-		if self.should_lower_text:
-			return text.lower()
-		else:
-			return text
-		
-	def play_game(self):
-		"""
-		Start playing the user-created game.
-		"""
-		while True:
-			map_position_data = self.map.data[self.map_position]
-			choices           = map_position_data.choices
-			
-			map_position_data.position_enter()
-			user_input = input(self.prompt)
-			
-			if self._conditional_lower(user_input) in choices:
-				self.map_position = choices[self._conditional_lower(user_input)]
-				map_position_data.position_exit()
-			else:
-				print(self.invalid_input_msg)
+    """
+    Stores general data about the game. A MainGame instance
+    contains the following attributes.
+    
+        name              - The name of the game.
+        desc              - The game's description.
+        prompt            - The global prompt to be used.
+        invalid_input_msg - A message to print if user enters invalid input.
+        map               - The game's map data, a GameMap instance.
+        should_lower_text - The option to lower text or not.
+    """
+    def __init__(self, name: str, desc: str, prompt: str, invalid_input_msg: str, map, should_lower_text: bool):
+        self.name              = name
+        self.desc              = desc
+        self.prompt            = prompt
+        self.invalid_input_msg = invalid_input_msg
+        self.map               = map
+        self.should_lower_text = should_lower_text
+        self.map_start         = self.map.find_start(self.should_lower_text)
+        self.map_position      = self.map_start
+        
+    def _conditional_lower(self, text: str):
+        if self.should_lower_text:
+            return text.lower()
+        else:
+            return text
+        
+    def play_game(self):
+        """
+        Start playing the user-created game.
+        """
+        while True:
+            map_position_data = self.map.data[self.map_position]
+            choices           = map_position_data.choices
+            
+            map_position_data.position_enter()
+            user_input = input(self.prompt)
+            
+            if self._conditional_lower(user_input) in choices:
+                self.map_position = choices[self._conditional_lower(user_input)]
+                map_position_data.position_exit()
+            else:
+                print(self.invalid_input_msg)
