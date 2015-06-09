985cb362d2d7602f01b7608582401d8e30ae355e
ShearOfDoom <ebr4d@live.com>
2015-06-09 17:04:24 -0500

    Added spaces in the test file for clarity.

--- TEST_test1.py
+++ TEST_test1.py
@@ -5,39 +5,39 @@ import cactus
 FLOWCHART = cactus.Flowchart({
     "data": [
         cactus.Position({
-            "name": "start",
+            "name":       "start",
             "desc_enter": "Description on enter.",
-            "desc_exit": "Description on exit.",
+            "desc_exit":  "Description on exit.",
             "choices": {
                 "choice1": 1,
                 "choice2": 2
             }
         }),
         cactus.Position({
-            "name": "Name Goes Here 1",
+            "name":       "Name Goes Here 1",
             "desc_enter": "Description on enter.",
-            "desc_exit": "Description on exit.",
-            "choices": {}
+            "desc_exit":  "Description on exit.",
+            "choices":    {}
         }),
         cactus.Position({
-            "name": "Name Goes Here 2",
+            "name":       "Name Goes Here 2",
             "desc_enter": "Description on enter.",
-            "desc_exit": "Description on exit.",
-            "choices": {}
+            "desc_exit":  "Description on exit.",
+            "choices":    {}
         })
     ]
 })
 
 
 GAME = cactus.Game({
-    "name": "Game Name",
-    "desc": "Game Description",
-    "prompt": "> ",
+    "name":              "Game Name",
+    "desc":              "Game Description",
+    "prompt":            "> ",
     "invalid_input_msg": "Invalid input",
-    "flowchart": FLOWCHART,
-    "case_sensitive": False,
-    "allow_help": True,
-    "about_text": "Write about your game here.",
+    "flowchart":         FLOWCHART,
+    "case_sensitive":    False,
+    "allow_help":        True,
+    "about_text":        "Write about your game here.",
     "event_handlers": {
         "position.name goes here 1.enter.after": exit,
         "position.name goes here 2.enter.after": exit
