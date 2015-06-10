3e91015da96b561129b6e921f4f5fa3097476584
ShearOfDoom <ebr4d@live.com>
2015-06-09 20:40:58 -0500

    Added some special error trace formatting.

--- cactus/errors.py
+++ cactus/errors.py
@@ -1,4 +1,7 @@
 from contextlib import redirect_stdout
+from traceback import format_exc
+from pprint import pformat
+from sys import exit
 
 def cactus_class_method_exception_handle(function):
     """
@@ -14,13 +17,16 @@ def cactus_class_method_exception_handle(function):
         try:
             return function(self, *args, **kwargs)
         except Exception as cactus_game_error:
+            print("Program has encountered an error. If you believe that this is")
+            print("engine-related, please report it to https://github.com/ShearOfDoom/Cactus/issues/")
+			print("To find full error stacktrace and details, see cactus_error_log.txt.")
             with open("cactus_error_log.txt", "w+") as cactus_error_log:
                 with redirect_stdout(cactus_error_log):
-                    print("Program has encountered an error. If you believe that this is an engine-related error, please report it at https://github.com/ShearOfDoom/Cactus/issues")
-                    print("Cactus error log")
-                    print("=" * 15)
-                    print("Class Data: " + self.class_data)
-                    print("=" * 15)
-                    print(str(cactus_game_error))
-                    print("=" * 15)
+                    print("ERROR")
+                    print("\n" + "=" * 45 + "\n")
+                    print("CLASS DATA:", pformat(self.class_data, indent=2))
+                    print("\n" + "=" * 45 + "\n")
+                    print("STACK TRACE:", format_exc())
+                    print("=" * 45 + "\n")
+                    exit(0)
     return wrapper
