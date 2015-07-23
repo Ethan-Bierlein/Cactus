49ed47cad6e50603eb96f03acc3378bcd34cc866
SanketDG <sanketdasgupta@gmail.com>
2015-07-23 23:37:52 +0530

    add basic setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+setup(
+    name='cactus',
+    packages=find_packages(),
+    version='0.2.0a',
+    description='A simple Python engine for creating text-based adventure games.',
+    author='Ethan Bierlein',
+    author_email='',
+    url='https://github.com/ShearOfDoom/Cactus',
+    download_url='https://github.com/ShearOfDoom/Cactus/archive/v0.2.0-alpha.zip',
+    keywords=[],
+    classifiers=[],
+)
